627a0697bfba5f8f4e6c7a48aad8fa5b9e5bce30
Harvar3105 <Petrochenko03@inbox.ru>
2024-06-08 17:57:46 +0300

    GithubUpload

---
+++ VirtualMe/WebApp/DTO/JWTResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApp.DTO;
+
+public class JWTResponse
+{
+    public string? UserId { get; set; } = default!;
+    public string Jwt { get; set; } = default!;
+    public string RefreshToken { get; set; } = default!;
+}
